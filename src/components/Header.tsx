71895a4f89ca0a677b658ee310a9f189c4dd6f2f
jossvan <vanessapolanco.tech@gmail.com>
2025-03-09 19:49:47 -0600

    create stores and getCategory

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { useAppStore } from "../stores/useAppStore";
 
 export default function Header() {
   const { pathname } = useLocation();
 
   const isHome = useMemo(() => pathname === "/", [pathname]);
 
+  const fetchCategories = useAppStore((state) => state.fetchCategories);
+  const categories = useAppStore((state) => state.categories);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
   return (
     <header
       className={isHome ? "bg-header bg-center bg-cover" : "bg-slate-800"}
@@ -70,6 +77,17 @@ export default function Header() {
                 className="p-3 w-full rounded-lg focus:outline-none bg-white"
               >
                 <option value="">-- Seleccionar --</option>
+
+                {categories.drinks.map((category) => {
+                  return (
+                    <option
+                      key={category.strCategory}
+                      value={category.strCategory}
+                    >
+                      {category.strCategory}
+                    </option>
+                  );
+                })}
               </select>
             </div>
 
